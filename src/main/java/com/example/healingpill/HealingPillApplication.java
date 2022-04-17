2b1335024b7680001c07d8a7a2f8896720f32552
lsh17831048 <ars8129@kyungmin.ac.kr>
2022-04-17 15:49:19 +0900

    HealingPill 프로젝트 기본 구성 Spring boot 2.6.6 JAVA 8 GRADLE

---
+++ src/main/java/com/example/healingpill/HealingPillApplication.java
@@ -0,0 +1,13 @@
+package com.example.healingpill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingPillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealingPillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/healingpill/HealingPillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healingpill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingPillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
